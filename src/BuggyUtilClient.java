d5ee341bb2d6c7bc6594f66e5df8afd0ee05c36d
jacob <jacobhughes133@gmail.com>
2018-10-05 19:26:03 -0400

    random change for lab

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -3,7 +3,7 @@ public class BuggyUtilClient {
     public static void main(String[] args) {
         // First test case in Main
 
-        // yo
+        // yo yo yo
 
         System.out.println("Example 1: Make My Username");
         System.out.println("In: first = " + "\"Sean\"" +
