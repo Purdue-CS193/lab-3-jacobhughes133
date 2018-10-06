8e6c069e99295184a29e71d843ea4b4d51edcad2
jacob <jacobhughes133@gmail.com>
2018-10-05 22:01:14 -0400

    some of tests are passing

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -3,7 +3,7 @@ public class MathUtils {
      * divideNumbers -- a / b
      * Divide a by b and return the result
      */
-    public static double divideNumbers(int dividend, int divisor) {
+    public static double divideNumbers(double dividend, double divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
         double res = dividend / divisor;
         /* Return the DEFINITELY CORRECT result */
@@ -14,14 +14,13 @@ public class MathUtils {
      * raiseToPower -- (base)^(exp)
      * Raise the number base to power exp
      */
-    public static double raiseToPower(int base, int exp) {
+    public static double raiseToPower(double base, double exp) {
         /* Set initial result to be just the number */
-        int res = base;
+        double res = base;
 
         /* Determine if the exponent is negative */
         boolean isNegative = false;
         if (exp < 0) {
-            exp *= -1;
             isNegative = true;
         }
 
@@ -29,12 +28,13 @@ public class MathUtils {
             Ex: base = 2, exp = 2
                --> res = 2, then res = 4 */
         for (int i = 0; i < exp; i++) {
-           res *= base; 
+           res = res * (base * exp);
         }
 
         /* If it's a negative exponent, we should invert it! */
         if (isNegative) {
-            double dividedRes = divideNumbers(1, res);
+
+            double dividedRes = 1 / res;
             return dividedRes;
         }
         /* Otherwise, we are safe to just return the result */
